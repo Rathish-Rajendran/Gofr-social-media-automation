b5b3d62ff4062f10a9995a4605c2e3fd176996cf
kamalraj-ten <kamalrajdhanakodi@gmail.com>
2024-11-23 19:35:43 +0530

    Add comment

--- service/main.go
+++ service/main.go
@@ -2,38 +2,38 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/joho/godotenv"
 	"gofr.dev/pkg/gofr"
 )
 
 func initSecrets() error {
-    // .secrets.env will contain any api access tokens
-    return godotenv.Load(".secrets.env")
+	// .secrets.env will contain any api access tokens
+	return godotenv.Load(".secrets.env")
 }
 
 func main() {
-
 	// Load secrets
 	err := initSecrets()
-    if err != nil {
-        fmt.Println("Cannot initialize the service - %w", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("Cannot initialize the service - %w", err)
+		return
+	}
 
-    // initialise gofr object
-    app := gofr.New()
-	
-    // register route greet
-    app.GET("/chat", func(ctx *gofr.Context) (interface{}, error) {
+	// initialise gofr object
+	app := gofr.New()
 
-        return "Hello World!", nil
-    })
+	// register route greet
+	app.GET("/chat", func(ctx *gofr.Context) (interface{}, error) {
+
+		return "Hello World!", nil
+	})
 
 	app.POST("/tweet", func(ctx *gofr.Context) (interface{}, error) {
-		return TweetHandler( ctx )
-	} )
+		return TweetHandler(ctx)
+	})
 
-    // Runs the server, it will listen on the default port 8000.
-    // it can be over-ridden through configs
-   app.Run()
+	// Runs the server, it will listen on the default port 8000.
+	// it can be over-ridden through configs
+	app.Run()
 }
