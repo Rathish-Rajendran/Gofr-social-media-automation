2c868e62034893a502986dcac026f7b455a3f6bd
Rathish-Rajendran <rathishrajendran9601@gmail.com>
2024-11-24 10:44:52 +0530

    Add Markdown

--- AI-Alchemy/src/components/contentpage.jsx
+++ AI-Alchemy/src/components/contentpage.jsx
@@ -1,27 +1,32 @@
-const ContentPage = ({ items, showItems=true, isMail=false }) => {
-    console.log(items)
-    return (<div className="container-fluid row g-4 mt-4">
-        { /* Show the card items */ }
-        { showItems &&
-            items.map((item, index) => (
-                <div key={index} className="card text-bg-dark m-1 col-md-5">
-                    <div className="card-body">
-                    <h5 className="card-title">{isMail? item.subject : item.heading}</h5>
-                    <p className="card-text">{item.body}</p>
-                    <a href="#" className="btn btn-primary"
-                        onClick={item.onSend}
-                    >Send</a>
-                    </div>
-                </div>
-            ))
-        }
-        { showItems && items.length === 0 &&
-            <h1>No items to show</h1>
-        }
+import Markdown from "react-markdown";
 
-        { /* Show the analytics */}
-        {!showItems && <h1>No analytics at the moment</h1>}
-    </ div>);
-}
+const ContentPage = ({ items, showItems = true, isMail = false }) => {
+  console.log(items);
+  return (
+    <div className="container-fluid row g-4 mt-4">
+      {/* Show the card items */}
+      {showItems &&
+        items.map((item, index) => (
+          <div key={index} className="card text-bg-dark m-1 col-md-5">
+            <div className="card-body">
+              <h5 className="card-title">
+                {isMail ? item.subject : item.heading}
+              </h5>
+              <div className="card-text">
+                <Markdown>{item.body}</Markdown>
+              </div>
+              <a href="#" className="btn btn-primary" onClick={item.onSend}>
+                Send
+              </a>
+            </div>
+          </div>
+        ))}
+      {showItems && items.length === 0 && <h1>Loading...</h1>}
+
+      {/* Show the analytics */}
+      {!showItems && <h1>No analytics at the moment</h1>}
+    </div>
+  );
+};
 
 export default ContentPage;
